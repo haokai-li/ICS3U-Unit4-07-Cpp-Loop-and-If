e925f85b8d816e28be872704e1a2d46e00ed3a7b
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-11 03:04:08 +0000

    loop and if

---
+++ loopAndIf.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 haokai All rights reserved
+//
+// Created by: Haokai
+// Created on: Oct 2021
+// This Program is about loop and if
+#include <iostream>
+#include <string>
+
+int main() {
+    // This Program is about loop and if
+    int loopNumber;
+
+    // process
+    for (loopNumber = 1000; loopNumber < 2001; loopNumber++) {
+        if ((loopNumber + 1) % 5 == 0) {
+            // output
+            std::cout << loopNumber << " " << std::endl;
+        } else {
+            // output
+            std::cout << loopNumber << " ";
+        }
+    }
+    std::cout << "\nDone." << std::endl;
+}
